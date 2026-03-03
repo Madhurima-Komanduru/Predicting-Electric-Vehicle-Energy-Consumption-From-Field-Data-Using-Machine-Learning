6533521063d82e927e2a4eb0a932826cc7974f09
Komanduru Madhurima <madhurimakomanduru@gmail.com>
2026-03-04 02:01:52 +0530

    Fixed SQLite database path for Django 2.1

--- EV_Energy_Consumption/settings.py
+++ EV_Energy_Consumption/settings.py
@@ -79,13 +79,20 @@ WSGI_APPLICATION = 'EV_Energy_Consumption.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+import os
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
